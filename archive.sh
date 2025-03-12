71838d6b1d6ff639c2659c6cb6f4cc15cf18b2aa
Josh Grosse <josh@jggimi.net>
2025-03-12 19:32:54 -0400

    bump archive.sh for obsd.4.1.0-beta.2

--- archive.sh
+++ archive.sh
@@ -3,12 +3,12 @@
 # archivers/xz and devel/git must be installed
 #
 # delete any old tarballs
-rm -r /tmp/transmission-4.1.0.dev* 2> /dev/null
+rm -r /tmp/transmission-4.1.0-beta.2* 2> /dev/null
 # archive main directory
-git archive --format=tar --prefix=transmission-4.1.0.dev/ HEAD | tar xf - -C /tmp
+git archive --format=tar --prefix=transmission-4.1.0-beta.2/ HEAD | tar xf - -C /tmp
 # descend recursively and archive each submodule
-git submodule --quiet foreach --recursive 'git archive --format=tar --prefix=transmission-4.1.0.dev/$path/ HEAD | tar xf - -C /tmp'
+git submodule --quiet foreach --recursive 'git archive --format=tar --prefix=transmission-4.1.0-beta.2/$path/ HEAD | tar xf - -C /tmp'
 # create the tarball, compress, move to distfiles
 cd /tmp
-tar cf transmission-4.1.0.dev.tar transmission-4.1.0.dev
-/usr/local/bin/xz transmission-4.1.0.dev.tar && doas mv transmission-4.1.0.dev.tar.xz /usr/ports/distfiles
+tar cf transmission-4.1.0-beta.2.tar transmission-4.1.0-beta.2
+/usr/local/bin/xz transmission-4.1.0-beta.2.tar && doas mv transmission-4.1.0-beta.2.tar.xz /usr/ports/distfiles
